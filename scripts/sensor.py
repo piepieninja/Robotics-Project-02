effd53560b672182eab1107373133238981ae697
Caleb Adams <piepieninja@gmail.com>
2018-10-12 15:14:37 -0400

    done w sensor maybe

--- scripts/sensor.py
+++ scripts/sensor.py
@@ -5,6 +5,7 @@ import random
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String, Float64
+from Robotics-Project-2.msg import Sensor_range
 
 def callback(data):
     print "yeet: "
@@ -16,9 +17,14 @@ def callback(data):
     mu = 0.0
     sigma = 1.0
     x_range = x + random.gauss(mu, sigma)
+    y_range = y + random.gauss(mu, sigma)
+    print "x + gaussian: " + str(x_range) + ", y + gaussian: " + str(y_range) 
     # publish values to the guy
-    pub = rospy.Publisher('sensor_x_range', Float64, queue_size=10)
-    pub.publish(x_range)
+    msg = Sensor_range
+    msg.z1 = x_range
+    msg.z2 = y_range
+    pub = rospy.Publisher('sensor_x_range', Sensor_range, queue_size=10)
+    pub.publish(msg)
 
     
 def listener():
