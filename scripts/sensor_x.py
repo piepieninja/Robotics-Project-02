5ed29c8275245ee13e7849449a52725b7007e31a
Caleb Adams <piepieninja@gmail.com>
2018-10-12 14:38:23 -0400

    asdf

--- scripts/sensor_x.py
+++ scripts/sensor_x.py
@@ -8,8 +8,17 @@ from std_msgs.msg import String
 def callback(data):
     print "yeet: "
     print data.pose.pose
+    x = odom.pose.pose.position.x
+    y = odom.pose.pose.position.y
+    z = odom.pose.pose.position.z
+    # do something ...
+    # ...
+    # profit?
+    
+    
     
 def listener():
+    print "pls"
     rospy.init_node('sensor_x', anonymous=True)    
     rospy.Subscriber("base_pose_ground_truth", Odometry, callback)
     rospy.spin()
