135fb635c52c05a6b8b4c699b8314196f0849e07
Caleb Adams <piepieninja@gmail.com>
2018-10-13 16:36:34 -0400

    updates to bayes guy

--- scripts/bayes_guy.py
+++ scripts/bayes_guy.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 import rospy
 import random
+import message_filters
 # imports
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String, Float64
 from Robotics_Project_02.msg import Sensor_range
 
-def callback(data):
+def callback(sensor_data,odom_data,control_data):
     print "yeet: "
-    print "z1: " + str(data.z1) + ", z2: " + str(data.z2)
-    # add in the randomness
+    print sensor_data
+    print odom_data
+    print control_data
+    x = sensor_data.z1
+    y = sensor_data.z2
+
     
 def listener():
     print "pls"
-    rospy.init_node('bayes_guy', anonymous=True)    
-    rospy.Subscriber("sensor_range", Sensor_range, callback)
+    sensor_sub  = message_filters.Subscriber('sensor_range', Sensor_range)
+    odom_sub    = message_filters.Subscriber('base_odometry/odom', Odometry)
+    control_sub = message_filters.Subscriber('base_controller/command', Twist)
+    
+    ts = message_filters.TimeSynchronizer([sensor_sub,odom_sub,control_sub], 10)
+    ts.registerCallback(callback)
+
     rospy.spin()
 
 if __name__ == '__main__':
