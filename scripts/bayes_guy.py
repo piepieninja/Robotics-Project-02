8942cbd3425be1b0519dbbaa725deb3029fdac49
Caleb Adams <piepieninja@gmail.com>
2018-10-13 17:17:35 -0400

    welp

--- scripts/bayes_guy.py
+++ scripts/bayes_guy.py
@@ -8,22 +8,25 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import String, Float64
 from Robotics_Project_02.msg import Sensor_range
 
+
+
+
 def callback(sensor_data,odom_data,control_data):
     print "yeet: "
     print sensor_data
     print odom_data
     print control_data
-    x = sensor_data.z1
-    y = sensor_data.z2
+    #x = sensor_data.z1
+    #y = sensor_data.z2
 
     
 def listener():
     print "pls"
     rospy.init_node('bayes_guy', anonymous=True)
     
-    sensor_sub  = message_filters.Subscriber('sensor_range', Sensor_range)
-    odom_sub    = message_filters.Subscriber('base_odometry/odom', Odometry)
-    control_sub = message_filters.Subscriber('base_controller/command', Twist)
+    sensor_sub  = message_filters.Subscriber("sensor_range", Sensor_range)
+    odom_sub    = message_filters.Subscriber("base_odometry/odom", Odometry)
+    control_sub = message_filters.Subscriber("base_controller/command", Twist)
     
     ts = message_filters.TimeSynchronizer([sensor_sub,odom_sub,control_sub], 10)
     ts.registerCallback(callback)
