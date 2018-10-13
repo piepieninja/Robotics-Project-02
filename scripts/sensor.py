310cbcb5ab5a61a2159553071e43d4bbaeeac02c
Caleb Adams <piepieninja@gmail.com>
2018-10-13 14:37:14 -0400

    asdf

--- scripts/sensor.py
+++ scripts/sensor.py
@@ -7,25 +7,30 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import String, Float64
 from Robotics_Project_02.msg import Sensor_range
 
+x_limit = 100
+y_limit = 100
+sensor_limit = 50
+
+def sense(true_x,true_y):
+    # calculate the distances w/ the sensor model
+    mu = 0.0
+    sigma = 1.0
+    x_range = (x_limit - true_x) + random.gauss(mu, sigma)
+    y_range = (y_limit - true_y) + random.gauss(mu, sigma)
+    # make the message type and fill it
+    ranges = Sensor_range()
+    ranges.z1 = x_range
+    ranges.z2 = y_range
+    # yeeto complete-o
+    return ranges
+
 def callback(data):
-    print "yeet: "
     print data.pose.pose
     x = data.pose.pose.position.x
     y = data.pose.pose.position.y
     z = data.pose.pose.position.z
-    # add in the randomness
-    mu = 0.0
-    sigma = 1.0
-    x_range = x + random.gauss(mu, sigma)
-    y_range = y + random.gauss(mu, sigma)
-    print "x + gaussian: " + str(x_range) + ", y + gaussian: " + str(y_range)     
-    # HERE DO THE ACTUAL RANGE FINDING
-    # modify x_range 
-    # modify y_range
-    # publish values to the guy
-    msg = Sensor_range()
-    msg.z1 = x_range
-    msg.z2 = y_range
+    msg = sense(x,y)
+    print msg
     pub = rospy.Publisher('sensor_range', Sensor_range, queue_size=10)
     pub.publish(msg)
 
