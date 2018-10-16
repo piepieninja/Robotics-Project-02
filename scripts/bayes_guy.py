d3bb2042bad49ab059a2304a69f22850dce6e93f
Christian-Ubuntu <caadkins@uga.edu>
2018-10-16 14:37:15 -0400

    my guy

--- scripts/bayes_guy.py
+++ scripts/bayes_guy.py
@@ -29,6 +29,10 @@ posterior_y[2] = 1.0
 #pub = rospy.Publisher("/BAYES_OUTPUT", Twist, queue_size=10) 
 gaussian_values = [.06, .24, 0.4, .24, .06]
 kernel = [.1, .8, .1]
+prevX = 2
+prevY = 2
+lastPoseX = 2
+lastPoseY = 2
 
 def obs_model(x, y):
     likelihood_x = np.zeros(100, dtype=float)
@@ -36,39 +40,95 @@ def obs_model(x, y):
     for i in range(x-2,100):
         if g_i == 5:
             break
+        if i < 0:
+	  continue
         likelihood_x[i] = gaussian_values[g_i]
         g_i += 1
     likelihood_y = np.zeros(100, dtype=float)
-    print 'likelihood_x:'
-    print likelihood_x
+    #print 'likelihood_x:'
+    #print likelihood_x
     g_i = 0;
     for i in range(x-2,100):
         if g_i == 5:
             break
+	if i < 0:
+	  continue
         likelihood_y[i] = gaussian_values[g_i]
         g_i += 1
-    print 'likelihood_y:'
-    print likelihood_y
+    #print 'likelihood_y:'
+    #print likelihood_y
     global prior_x
     global prior_y
     global posterior_x
     global posterior_y
-    prior_x = predict(posterior_x,1,gaussian_values)
-    prior_y = predict(posterior_y,1,gaussian_values)
+    prior_x = predict(posterior_x,x-prevX,gaussian_values)
+    prior_y = predict(posterior_y,y-prevY,gaussian_values)
+    posterior_x = update(likelihood_x, prior_x)    
+    posterior_y = update(likelihood_y, prior_y)
+    #print 'post x:'
+    #print posterior_x
+    #print 'post y:'
+    #print posterior_y
+    global prevX
+    global prevY
+    prevX = x
+    prevY = y
+
+def motion_model_update(axis, distance):
+    space = 0
+    xOffset = 0
+    yOffset = 0
+    likelihood_x = np.zeros(100, dtype=float)
+    g_i = 0;
+    if axis == "x":
+      xOffset = distance
+      space = 2 * distance
+      for i in range(lastPoseX+space,100):
+        if g_i == 3:
+          break
+        if i < 0:
+	  continue
+        likelihood_x[i] = kernel[g_i]
+        g_i += 1
+    else:
+      likelihood_x[lastPoseX] = 1
+    print 'likelihood_x:'
+    print likelihood_x
+    
+    space = 0
+    likelihood_y = np.zeros(100, dtype=float)
+    g_i = 0;
+    if axis == "y":
+      yOffset = distance
+      space = 2 * distance
+      for i in range(lastPoseY+space,100):
+          if g_i == 3:
+              break
+	  if i < 0:
+	    continue
+          likelihood_y[i] = kernel[g_i]
+          g_i += 1
+    else:
+      likelihood_y[lastPoseY] = 1
+    print 'likelihood_y:'
+    print likelihood_y
+
+    global prior_x
+    global prior_y
+    global posterior_x
+    global posterior_y
+    prior_x = predict(posterior_x,xOffset,kernel)
+    prior_y = predict(posterior_y,yOffset,kernel)
     posterior_x = update(likelihood_x, prior_x)    
     posterior_y = update(likelihood_y, prior_y)
     print 'post x:'
     print posterior_x
     print 'post y:'
     print posterior_y
-
-def motion_model_update(axis, distance):
-    if axis == "x":
-        prior_x = predict(posterior_x, distance, kernel)
-        #posterior_X = update()
-    if axis == "y":
-        prior_y = predict(posterior_y, distance, kernel)
-
+    global lastMovPoseX
+    global lastMovPoseY
+    lastPoseX += distance
+    lastPoseY += distance
     
 def callback(sensor_data, control_data):
     print "yeet: "
